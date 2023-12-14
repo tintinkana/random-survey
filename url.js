1b1d4a2e6cf12a724982271e3147b2c02030d787
tintinkana <141254245+tintinkana@users.noreply.github.com>
2023-12-14 14:53:44 +0900

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://zhuanlan.zhihu.com/p/498718553',
+	'https://forms.gle/Vd6uzEGj8JQM77Mj6',
 	'https://ssdd.site/statistics-writing/2018/04/15/%E4%BD%BF%E7%94%A8G-power%E8%AE%A1%E7%AE%97%E6%95%88%E5%BA%94%E9%87%8F%E5%92%8C%E6%A0%B7%E6%9C%AC%E9%87%8F.html',
 	'http://www.jieyoutsg.com/e/action/ListInfo/?classid=63'
 ];
